ecb1be374d678ff7672829f96bdfb936ce232d7c
SovaV <sova.volodymyr95@gmail.com>
2021-09-29 12:34:30 +0300

    shortid

--- src/component/Feedback/FeedbackOptions .js
+++ src/component/Feedback/FeedbackOptions .js
@@ -1,13 +1,15 @@
 import f from './FeedbackOptions.module.css';
 import PropTypes from 'prop-types';
+import shortid from 'shortid';
+
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <>
       {options.map(option => (
         <button
+          key={shortid.generate()}
           className={f.btn}
           type="button"
-          key={option}
           onClick={() => onLeaveFeedback(option)}
         >
           {option}
